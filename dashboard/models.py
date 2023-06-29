90c2f07d17b9ecd0ffd7066918e2574beaa4ab72
Marcos Mendez Quintero <marcos.mendez.quintero@fing.edu.uy>
2023-06-29 02:43:06 -0300

    Googls Ads + MetaAds + CountryCode

--- dashboard/models.py
+++ dashboard/models.py
@@ -6,9 +6,9 @@ from django_countries.fields import CountryField
 class Empresa(models.Model):
     nombre = models.CharField(max_length=200)
     pais = CountryField(blank_label='(selecciona país)', null=True)  # ISO 3166 for Country Codes
-    google_ads = models.CharField(max_length=200, null=True)
-    meta_ads = models.CharField(max_length=200, null=True)
-    usuarios = models.ManyToManyField(User)
+    google_ads = models.CharField(max_length=200, blank=True, null=True)
+    meta_ads = models.CharField(max_length=200, blank=True, null=True)
+    usuarios = models.ManyToManyField(User, blank=True)
 
     def __str__(self):
         return self.nombre
@@ -17,7 +17,6 @@ class Empresa(models.Model):
 class Competencia(models.Model):
     empresa = models.ForeignKey(Empresa, related_name='empresa', on_delete=models.CASCADE)
     competidor = models.ForeignKey(Empresa, related_name='competidor', on_delete=models.CASCADE)
-    #google_ads = models.ForeignKey(Empresa, related_name='google_ads', on_delete=models.CASCADE)
 
     class Meta:
         unique_together = ('empresa', 'competidor')
